77baeaa468b28c090adac2ac0436a39d4e8dd9d8
Arun Kumar <30272820+arun94255@users.noreply.github.com>
2018-07-13 14:35:43 +0530

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+#from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from . forms import SignUpForm
+
+
+@login_required
+def home(request):
+    return render(request, 'accounts/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
